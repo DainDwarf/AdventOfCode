979f3624bd96f6b9b9873f5be31dae017a2e37f0
DainDwarf <adrien.panhaleux@ens-lyon.org>
2017-12-07 22:32:08 +0100

    Adding Day7, partTwo solution, but it is ugly.

--- Day7/day7.py
+++ Day7/day7.py
@@ -2,7 +2,7 @@
 from __future__ import print_function
 import re
 
-class Tower(object):
+class TowerParse(object):
     def __init__(self, line):
         if '->' in line:
             head, deps = line.split('->')
@@ -20,10 +20,45 @@ class Tower(object):
             return "{n} ({w}) -> {d}".format(n=self.name, w=self.weight, d=str(self.deps))
         else:
             return "{n} ({w})".format(n=self.name, w=self.weight)
-        
-def toTowerList(text):
-    return list(map(Tower, text.strip().split('\n')))
 
+
+class Tower(object):
+    def __init__(self, name, weight, deps):
+        self.name = name
+        self.weight = weight
+        self.deps = deps
+
+    def __str__(self):
+        if self.deps:
+            return "{n} ({w}) -> {d}".format(n=self.name, w=self.weight, d=self.deps.name)
+        else:
+            return "{n} ({w})".format(n=self.name, w=self.weight)
+
+    def __hash__(self):
+        return hash(name)
+
+    def realWeight(self):
+        return self.weight + sum(map(lambda d: d.realWeight(), self.deps))
+
+    def isBalanced(self):
+        if self.deps:
+            return all(map(lambda d: d.realWeight() == self.deps[0].realWeight(), self.deps))
+        else:
+            return True
+
+def toTowerList(text):
+    return list(map(TowerParse, text.strip().split('\n')))
+
+
+def toTowerTree(text):
+    towerParseList = toTowerList(text)
+    parseDict = dict( (t.name, t) for t in towerParseList)
+
+    def _turnToTree(parseNode):
+        return Tower(parseNode.name, parseNode.weight, list(map(lambda n: _turnToTree(parseDict[n]), parseNode.deps)))
+
+    head = getHead(towerParseList)
+    return _turnToTree(head)
 
 def getHead(towerList):
     for t in towerList:
@@ -48,6 +83,7 @@ cntj (57)
 """
 
     print("Head of example towers is {t}".format(t=partOne(example)))
+    partTwo(example)
 
 
 def partOne(inp):
@@ -55,7 +91,22 @@ def partOne(inp):
     return getHead(towers)
 
 def partTwo(inp):
-    pass
+    tree = toTowerTree(inp)
+    allNodes = []
+
+    def _addNodes(node):
+        allNodes.append(node)
+        for n in node.deps:
+            _addNodes(n)
+    _addNodes(tree)
+
+    for n in allNodes:
+        if not n.isBalanced() and all(map(lambda d: d.isBalanced(), n.deps)):
+            print("Node {name} is wrong!".format(name=n.name))
+            for d in n.deps:
+                print("Dependancy {name} of weight {w} has realweight {r}".format(name=d.name, w=d.weight, r=d.realWeight()))
+
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, FileType
