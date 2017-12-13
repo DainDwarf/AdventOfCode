30fa71a8c2edea4fc65cedac2d980482068fc013
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2017-12-13 08:19:58 +0100

    WIP for 2015-19.

--- 2015/Day19/day19.py
+++ 2015/Day19/day19.py
@@ -14,6 +14,9 @@ def genAllSubstitutions(molecule, rules):
 def getAllRules(inp):
     return list(l.split(' => ') for l in inp.split('\n'))
 
+def getAllReverseRules(inp):
+    return list(map(lambda l: (l.strip().split(' => ')[1], l.strip().split(' => ')[0]),inp.split('\n')))
+
 
 # That's handy, the Advent of Code gives unittests.
 def UnitTest():
@@ -27,17 +30,71 @@ O => HH"""
     print("Test {inp} gives {res}".format(inp=ex1, res=partOne(ex1, ex_rules)))
     print("Test {inp} gives {res}".format(inp=ex2, res=partOne(ex2, ex_rules)))
 
+    ex2_rules = """e => H
+e => O
+H => HO
+H => OH
+O => HH"""
+
     print("")
     print("Unit test for Part Two.")
-    # print("Test {inp} gives {res}".format(inp=ex, res=partTwo(ex)))
+    print("Test {inp} gives {res}".format(inp=ex1, res=twoWaysPartTwo(ex1, ex2_rules)))
+    print("Test {inp} gives {res}".format(inp=ex2, res=twoWaysPartTwo(ex2, ex2_rules)))
 
+
+def step(s, rules):
+    ret = set()
+    for m in s:
+        for su in genAllSubstitutions(m, rules):
+            ret.add(su)
+    return ret
 
 def partOne(molecule, rules_str):
     rules = getAllRules(rules_str)
     return len(set(genAllSubstitutions(molecule, rules)))
 
-def partTwo(inp):
-    pass
+def partTwo(molecule, rules_str):
+    rules = getAllRules(rules_str)
+    current_mols = set('e')
+    steps = 0
+    while molecule not in current_mols:
+        print("Testing step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
+        current_mols = step(current_mols, rules)
+        steps += 1
+    return steps
+
+def reversePartTwo(molecule, rules_str):
+    rules = getAllReverseRules(rules_str)
+    current_mols = set([molecule])
+    steps = 0
+    while 'e' not in current_mols:
+        print("Testing step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
+        current_mols = step(current_mols, rules)
+        steps += 1
+    return steps
+
+def twoWaysPartTwo(molecule, rules_str):
+    rules = getAllRules(rules_str)
+    current_mols = set(['e'])
+    rev_rules = getAllReverseRules(rules_str)
+    rev_current_mols = set([molecule])
+    steps = 0
+    while len(current_mols.intersection(rev_current_mols)) == 0:
+        print("Testing forward step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
+        current_mols = step(current_mols, rules)
+        steps += 1
+        if len(current_mols.intersection(rev_current_mols)) != 0:
+            break
+        print("Testing forward step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
+        current_mols = step(current_mols, rules)
+        steps += 1
+        if len(current_mols.intersection(rev_current_mols)) != 0:
+            break
+        print("Testing backward step {s} on {l} molecules.".format(s=steps, l=len(rev_current_mols)))
+        rev_current_mols = step(rev_current_mols, rev_rules)
+        steps += 1
+    return steps
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, FileType
@@ -53,4 +110,4 @@ if __name__ == '__main__':
     if options.input:
         inp = options.input.read().strip()
         print("Answer for part one is : {res}".format(res=partOne(options.molecule, inp)))
-        # print("Answer for part two is : {res}".format(res=partTwo(inp)))
+        print("Answer for part two is : {res}".format(res=twoWaysPartTwo(options.molecule, inp)))
