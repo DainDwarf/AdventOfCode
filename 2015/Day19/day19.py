c377565a247bf70f9105eb7e5a6027e293cf1b3f
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2017-12-15 15:23:58 +0100

    Cleaning up work files.

--- 2015/Day19/day19.py
+++ 2015/Day19/day19.py
@@ -17,6 +17,48 @@ def getAllRules(inp):
 def getAllReverseRules(inp):
     return list(map(lambda l: (l.strip().split(' => ')[1], l.strip().split(' => ')[0]),inp.split('\n')))
 
+def Levenshtein(a, b):
+    """Computes Levenshtein distance, to use as heuristic for Astar.
+
+    Iterative algorithm from Wikipedia."""
+    v0 = list(range(len(b)+1))
+    v1 = list(range(len(b)+1)) # Or whatever.
+
+    for i in range(len(a)):
+        v1[0] = i + 1
+
+        for j in range(len(b)):
+            deletionCost = v0[j + 1] + 1
+            insertionCost = v1[j] + 1
+            substitutionCost = v0[j] if a[i] == b[j] else v0[j]+1
+            v1[j + 1] = min(deletionCost, insertionCost, substitutionCost)
+
+        v1, v0 = v0, v1
+    return v0[len(b)]
+
+
+def AStar(start, end, rules):
+    closedSet = set()
+    gScore = {start: 0}
+    fScore = {start: Levenshtein(start, end)}
+
+    while fScore:
+        current, _ = min(fScore.items(), key=lambda i:i[1])
+        if current == end:
+            return gScore[current]
+
+        fScore.pop(current)
+        closedSet.add(current)
+        for neighbor in set(genAllSubstitutions(current, rules)):
+            if neighbor in closedSet:
+                continue
+
+            tentative_gScore = gScore[current] + 1
+            if neighbor in gScore and tentative_gScore >= gScore[neighbor]:
+                continue
+
+            gScore[neighbor] = tentative_gScore
+            fScore[neighbor] = tentative_gScore + Levenshtein(neighbor, end)
 
 # That's handy, the Advent of Code gives unittests.
 def UnitTest():
@@ -38,63 +80,18 @@ O => HH"""
 
     print("")
     print("Unit test for Part Two.")
-    print("Test {inp} gives {res}".format(inp=ex1, res=twoWaysPartTwo(ex1, ex2_rules)))
-    print("Test {inp} gives {res}".format(inp=ex2, res=twoWaysPartTwo(ex2, ex2_rules)))
+    print("Test {inp} gives {res}".format(inp=ex1, res=partTwo(ex1, ex2_rules)))
+    print("Test {inp} gives {res}".format(inp=ex2, res=partTwo(ex2, ex2_rules)))
 
-
-def step(s, rules):
-    ret = set()
-    for m in s:
-        for su in genAllSubstitutions(m, rules):
-            ret.add(su)
-    return ret
 
 def partOne(molecule, rules_str):
     rules = getAllRules(rules_str)
     return len(set(genAllSubstitutions(molecule, rules)))
 
 def partTwo(molecule, rules_str):
-    rules = getAllRules(rules_str)
-    current_mols = set('e')
-    steps = 0
-    while molecule not in current_mols:
-        print("Testing step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
-        current_mols = step(current_mols, rules)
-        steps += 1
-    return steps
-
-def reversePartTwo(molecule, rules_str):
+    # It goes much faster to go in reverse, for this.
     rules = getAllReverseRules(rules_str)
-    current_mols = set([molecule])
-    steps = 0
-    while 'e' not in current_mols:
-        print("Testing step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
-        current_mols = step(current_mols, rules)
-        steps += 1
-    return steps
-
-def twoWaysPartTwo(molecule, rules_str):
-    rules = getAllRules(rules_str)
-    current_mols = set(['e'])
-    rev_rules = getAllReverseRules(rules_str)
-    rev_current_mols = set([molecule])
-    steps = 0
-    while len(current_mols.intersection(rev_current_mols)) == 0:
-        print("Testing forward step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
-        current_mols = step(current_mols, rules)
-        steps += 1
-        if len(current_mols.intersection(rev_current_mols)) != 0:
-            break
-        print("Testing forward step {s} on {l} molecules.".format(s=steps, l=len(current_mols)))
-        current_mols = step(current_mols, rules)
-        steps += 1
-        if len(current_mols.intersection(rev_current_mols)) != 0:
-            break
-        print("Testing backward step {s} on {l} molecules.".format(s=steps, l=len(rev_current_mols)))
-        rev_current_mols = step(rev_current_mols, rev_rules)
-        steps += 1
-    return steps
-
+    return AStar(molecule, 'e', rules)
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, FileType
@@ -110,4 +107,4 @@ if __name__ == '__main__':
     if options.input:
         inp = options.input.read().strip()
         print("Answer for part one is : {res}".format(res=partOne(options.molecule, inp)))
-        print("Answer for part two is : {res}".format(res=twoWaysPartTwo(options.molecule, inp)))
+        print("Answer for part two is : {res}".format(res=partTwo(options.molecule, inp)))
