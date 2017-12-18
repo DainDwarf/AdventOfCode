05cf03e2020d3853392ab7b119fbe6c0b5a1da1d
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2017-12-18 16:50:41 +0100

    Adding solution for 2017-18, part two.

--- 2017/Day18/day18.py
+++ 2017/Day18/day18.py
@@ -1,63 +1,97 @@
 #!/usr/bin/python3
 from __future__ import print_function
+from queue import Queue, Empty
 
 class Emulator(object):
     def __init__(self, instructions):
-        self.__reg = dict((l, 0) for l in 'abcdefghijklmnopqrstuvwxyz')
-        self.__reg['sound'] = 0
-        self.__int = list(i.strip().split() for i in instructions.split('\n'))
-        self.__cur = 0
+        self._reg = dict((l, 0) for l in 'abcdefghijklmnopqrstuvwxyz')
+        self._reg['sound'] = 0
+        self._int = list(i.strip().split() for i in instructions.split('\n'))
+        self._cur = 0
 
     def __getitem__(self, k):
         try:
-            return self.__reg[k]
+            return self._reg[k]
         except KeyError:
             return int(k)
 
     def snd(self, x):
-        self.__reg['sound'] = self[x]
-        self.__cur += 1
+        self._reg['sound'] = self[x]
+        self._cur += 1
 
     def set(self, x, y):
-        self.__reg[x] = self[y]
-        self.__cur += 1
+        self._reg[x] = self[y]
+        self._cur += 1
 
     def add(self, x, y):
-        self.__reg[x] += self[y]
-        self.__cur += 1
+        self._reg[x] += self[y]
+        self._cur += 1
 
     def mul(self, x, y):
-        self.__reg[x] *= self[y]
-        self.__cur += 1
+        self._reg[x] *= self[y]
+        self._cur += 1
 
     def mod(self, x, y):
-        self.__reg[x] %= self[y]
-        self.__cur += 1
+        self._reg[x] %= self[y]
+        self._cur += 1
 
     def rcv(self, x):
         if self[x] != 0:
-            return self.__reg['sound']
-        self.__cur += 1
+            return self._reg['sound']
+        self._cur += 1
 
     def jgz(self, x, y):
         if self[x] > 0:
-            self.__cur += self[y]
+            self._cur += self[y]
         else:
-            self.__cur += 1
+            self._cur += 1
+
+    def doStep(self, debug=False):
+        if debug:
+            print(self._int[self._cur])
+        instruction, *args = self._int[self._cur]
+        return getattr(self, instruction)(*args)
 
     def emulateUntilRcv(self):
         ret = None
         while True:
-            instruction, *args = self.__int[self.__cur]
-            ret = getattr(self, instruction)(*args)
+            ret = self.doStep()
             if ret is not None:
                 break
         return ret
 
 
+class ConcurrentEmulator(Emulator):
+    def __init__(self, instructions, programID, out_channel, in_channel):
+        super().__init__(instructions)
+        self._reg['p'] = programID
+        self._reg.pop('sound')
+        self.out_ch = out_channel
+        self.in_ch = in_channel
+        self.send_count = 0
+        self._wait = False
+
+    def snd(self, x):
+        self.out_ch.put_nowait(self[x])
+        self._cur += 1
+        self.send_count += 1
+
+    def rcv(self, x):
+        try:
+            self._reg[x] = self.in_ch.get_nowait()
+            self._cur += 1
+        except Empty:
+            self._wait = True
+
+    def isWaiting(self):
+        return self._wait
+
+    def getSendCount(self):
+        return self.send_count
+
 # That's handy, the Advent of Code gives unittests.
 def UnitTest():
-    ex = """set a 1
+    ex1 = """set a 1
 add a 2
 mul a a
 mod a 5
@@ -69,11 +103,19 @@ set a 1
 jgz a -2""".strip()
 
     print("Unit test for Part One.")
-    print("Test {inp} gives {res}".format(inp=ex, res=partOne(ex)))
+    print("Test {inp} gives {res}".format(inp=ex1, res=partOne(ex1)))
+
+    ex2 = """snd 1
+snd 2
+snd p
+rcv a
+rcv b
+rcv c
+rcv d""".strip()
 
     print("")
     print("Unit test for Part Two.")
-    print("Test {inp} gives {res}".format(inp=ex, res=partTwo(ex)))
+    print("Test {inp} gives {res}".format(inp=ex2, res=partTwo(ex2)))
 
 
 def partOne(inp):
@@ -82,7 +124,15 @@ def partOne(inp):
 
 
 def partTwo(inp):
-    pass
+    zeroToOne = Queue()
+    oneToZero = Queue()
+    emu0 = ConcurrentEmulator(inp, 0, zeroToOne, oneToZero)
+    emu1 = ConcurrentEmulator(inp, 1, oneToZero, zeroToOne)
+    while (not emu0.isWaiting()) or (not emu1.isWaiting()):
+        emu0.doStep()
+        emu1.doStep()
+    return emu1.getSendCount()
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser, FileType
