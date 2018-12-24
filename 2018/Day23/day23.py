e0bde913dfd1691e7065e8f474b41ee245f78a4f
Adrien Panhaleux <adrien.panhaleux@people-doc.com>
2018-12-24 14:25:46 +0100

    Starting a dichotomy using octahedrons, but I guess that sould fail...

--- 2018/Day23/day23.py
+++ 2018/Day23/day23.py
@@ -1,8 +1,20 @@
 import re
+import math
 
 
 def manhattan(p1, p2):
     return sum(map(lambda t: abs(t[0]-t[1]), zip(p1, p2)))
+
+
+class Octahedron(object):
+    """The bots define octahedrons, also called 3-ball for the L1 metric."""
+    def __init__(self, pos, r):
+        self.pos = pos
+        self.r = r
+
+    def intersect(self, other):
+        """Intersection can simply be tested by mesuring the distance between the centers."""
+        return manhattan(self.pos, other.pos) <= self.r+other.r
 
 
 # That's handy, the Advent of Code gives unittests.
@@ -26,7 +38,12 @@ pos=<1,3,1>, r=1"""
 def testTwo():
     print("Unit test for Part Two.")
 
-    inp = "toto"
+    inp = """pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5"""
     res = partTwo(inp)
     print(f"Test {inp} gives {res}")
 
@@ -42,7 +59,35 @@ def partOne(inp):
 
 
 def partTwo(inp):
-    pass
+    num_re = r"(-?\d+)"
+    bots = []
+    for line in inp.split('\n'):
+        x, y, z, r = list(map(int, re.findall(num_re, line)))
+        bots.append(Octahedron((x, y, z), r))
+    max_range = max(b.r for b in bots)
+    #Avoid issues by rounding to next power of 2
+    max_range = 2**math.ceil(math.log2(max_range))
+
+
+    #Now, some dichotomy:
+    search_bots = [Octahedron((0, 0, 0), max_range)]
+    current_r = max_range
+    while current_r > 1:
+        print("Range {r}, searching through {num} subspaces".format(r=current_r, num=len(search_bots)))
+        sub_search = []
+        sub_r = current_r//2
+        for bot in search_bots:
+            x, y, z = bot.pos
+            sub_search.append(Octahedron((x+sub_r, y, z), sub_r))
+            sub_search.append(Octahedron((x-sub_r, y, z), sub_r))
+            sub_search.append(Octahedron((x, y+sub_r, z), sub_r))
+            sub_search.append(Octahedron((x, y-sub_r, z), sub_r))
+            sub_search.append(Octahedron((x, y, z+sub_r), sub_r))
+            sub_search.append(Octahedron((x, y, z-sub_r), sub_r))
+            #There are two missing?
+        search_bots = sub_search
+        current_r = sub_r
+        raise NotImplementedError("Nope")
 
 
 if __name__ == '__main__':
