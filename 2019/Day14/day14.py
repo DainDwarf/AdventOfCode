ea283dea39529602241cfc1aa65c14db327fd5c7
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2019-12-16 12:59:47 +0100

    2019, day14.

---
+++ 2019/Day14/day14.py
@@ -0,0 +1,179 @@
+import pytest
+import math
+from itertools import count
+
+from networkx import DiGraph
+from networkx.algorithms.dag import is_directed_acyclic_graph, topological_sort
+
+
+# That's handy, the Advent of Code gives unittests.
+@pytest.mark.parametrize("inp, exp", [
+    ("""
+10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL""", 31),
+    ("""
+9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL""", 165),
+    ("""
+157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT""", 13312),
+    ("""
+2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF""", 180697),
+    ("""
+171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX""", 2210736),
+])
+def testOne(inp, exp):
+    res = partOne(inp)
+    print(f"Test {inp} gives {res}")
+    assert res == exp
+
+
+@pytest.mark.parametrize("inp, exp", [
+    ("""
+157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT""", 82892753),
+    ("""
+2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF""", 5586022),
+    ("""
+171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX""", 460664),
+])
+def testTwo(inp, exp):
+    res = partTwo(inp)
+    print(f"Test {inp} gives {res}")
+    assert res == exp
+
+
+def create_tree(inp):
+    tree = DiGraph()
+    for line in inp.strip().split('\n'):
+        lhs, rhs = line.strip().split(' => ')
+        rhs_qty, rhs_mol = rhs.strip().split()
+        for op in lhs.strip().split(', '):
+            qty, mol = op.strip().split()
+            tree.add_edge(rhs_mol, mol, output_qty=int(rhs_qty), input_qty=int(qty))
+    assert is_directed_acyclic_graph(tree)
+    return tree
+
+
+def ore_for_fuel(tree, need=1):
+    """Computes how much ore is needed for ``need`` fuel."""
+    for node in tree.nodes:
+        tree.nodes[node]['need'] = 0
+    tree.nodes['FUEL']['need'] = need
+    for node in topological_sort(tree):
+        for output_node, input_node, stoichio in tree.out_edges(node, data=True):
+            times = int(math.ceil(tree.nodes[output_node]['need']/stoichio['output_qty']))
+            tree.nodes[input_node]['need'] += times*stoichio['input_qty']
+    return tree.nodes['ORE']['need']
+
+
+def partOne(inp):
+    tree = create_tree(inp)
+    return ore_for_fuel(tree)
+
+
+def partTwo(inp):
+    """Solve by dichotomy."""
+    ore_limit = 1000000000000
+    tree = create_tree(inp)
+    for too_high in count():
+        if ore_for_fuel(tree, 2**too_high) > ore_limit:
+            break
+    low = 2**(too_high-1)
+    high = 2**too_high
+    while high-low > 1:
+        mid = (high+low)//2
+        if ore_for_fuel(tree, mid) > ore_limit:
+            high = mid
+        else:
+            low = mid
+    return low
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, FileType
+
+    args = ArgumentParser()
+    args.add_argument("input", help='Your input file', type=FileType('r'))
+    options = args.parse_args()
+
+    inp = options.input.read().strip()
+    print("Answer for part one is : {res}".format(res=partOne(inp)))
+    print("Answer for part two is : {res}".format(res=partTwo(inp)))
