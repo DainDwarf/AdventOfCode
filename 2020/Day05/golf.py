d320f9863e9540a21f07f3b14645d038ede9b236
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2020-12-05 11:10:58 +0100

    Pre-compute maketrans, because it's one char shorter.

--- 2020/Day05/golf.py
+++ 2020/Day05/golf.py
@@ -1,2 +1,2 @@
-s=set(int(l.translate(l.maketrans('BFRL','1010')),2)for l in open("i"))
+s=set(int(l.translate({66:49,70:48,82:49,76:48}),2)for l in open("i"))
 print(max(s),set(range(min(s),max(s)))-s)
