56df19aa8db18f301b482b94efd0f4d499d38646
Adrien Panhaleux <adrien.panhaleux@ens-lyon.org>
2020-12-05 11:07:38 +0100

    base= is optional in int()

--- 2020/Day05/golf.py
+++ 2020/Day05/golf.py
@@ -1,2 +1,2 @@
-s=set(int(l.translate(l.maketrans('BFRL','1010')),base=2)for l in open("i"))
+s=set(int(l.translate(l.maketrans('BFRL','1010')),2)for l in open("i"))
 print(max(s),set(range(min(s),max(s)))-s)
