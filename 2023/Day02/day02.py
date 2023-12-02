6f201227ce79c229eef6856c0c8eddb594746601
DainDwarf <daindwarf@gmail.com>
2023-12-02 15:28:33 +0100

    Add a nice solution for part2, day2!

--- 2023/Day02/day02.py
+++ 2023/Day02/day02.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 from collections import defaultdict
 from enum import StrEnum, auto
+from functools import reduce
+import operator
 import pytest
 
 
@@ -31,9 +33,14 @@ def test_set_is_valid(inp, exp):
 
 
 @pytest.mark.parametrize("inp, exp", [
+    ("3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", {'red': 4, 'green': 2, 'blue': 6}),
+    ("1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", {'red': 1, 'green': 3, 'blue': 4}),
+    ("8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", {'red': 20, 'green': 13, 'blue': 6}),
+    ("1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", {'red': 14, 'green': 3, 'blue': 15}),
+    ("6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", {'red': 6, 'green': 3, 'blue': 2}),
 ])
-def test_two(inp, exp):
-    res = part_two(inp)
+def test_minimal_set(inp, exp):
+    res = minimal_set(inp)
     assert res == exp
 
 
@@ -43,7 +50,7 @@ class Color(StrEnum):
     BLUE = auto()
 
 
-def to_set(inp: str):
+def to_dict(inp: str):
     ret = defaultdict(int)
     for cube in inp.split(', '):
         num, color = cube.split()
@@ -54,8 +61,7 @@ def to_set(inp: str):
 
 
 def set_is_valid(inp:str):
-    check = to_set(inp)
-    print(check)
+    check = to_dict(inp)
     return check[Color.RED] <= 12 and \
            check[Color.GREEN] <= 13 and \
            check[Color.BLUE] <= 14
@@ -64,6 +70,14 @@ def set_is_valid(inp:str):
 def game_is_valid(line):
     all_sets = line.split('; ')
     return all(set_is_valid(s) for s in all_sets)
+
+
+def minimal_set(line):
+    minimal = defaultdict(int)
+    for grab in line.split('; '):
+        for color, num in to_dict(grab).items():
+            minimal[color] = max(num, minimal[color])
+    return minimal
 
 
 def part_one(inp):
@@ -76,7 +90,11 @@ def part_one(inp):
 
 
 def part_two(inp):
-    pass
+    ret = 0
+    for line in inp.split('\n'):
+        line = line.split(': ')[1]
+        ret += reduce(operator.mul, minimal_set(line).values(), 1)
+    return ret
 
 
 if __name__ == '__main__':
