2c31fe7797ae699e122376ebb1645924192fc57d
DainDwarf <daindwarf@gmail.com>
2023-12-04 10:58:46 +0100

    day04, part one

---
+++ 2023/Day04/day04.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import pytest
+
+
+# That's handy, the Advent of Code gives unittests.
+@pytest.mark.parametrize("inp, exp", [
+    ("41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8),
+    ("13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2),
+    (" 1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2),
+    ("41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1),
+    ("87 83 26 28 32 | 88 30 70 12 93 22 82 36", 0),
+    ("31 18 13 56 72 | 74 77 10 23 35 67 36 11", 0),
+])
+def test_score(inp, exp):
+    res = score(inp)
+    assert res == exp
+
+
+@pytest.mark.parametrize("inp, exp", [
+])
+def test_two(inp, exp):
+    res = part_two(inp)
+    assert res == exp
+
+
+def score(card):
+    wins, nums = card.split('|')
+    wins = [int(w) for w in wins.split()]
+    nums = [int(n) for n in nums.split()]
+
+    ret = 0
+    for n in nums:
+        if n in wins:
+            ret += (ret if ret != 0 else 1)
+    return ret
+
+
+def part_one(inp):
+    ret = 0
+    for line in inp.split('\n'):
+        card = line.split(':')[1]
+        ret += score(card)
+    return ret
+
+
+def part_two(inp):
+    pass
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser, FileType
+
+    args = ArgumentParser()
+    args.add_argument("input", help='Your input file', type=FileType('r'))
+    options = args.parse_args()
+
+    inp = options.input.read().strip()
+    print("Answer for part one is : {res}".format(res=part_one(inp)))
+    print("Answer for part two is : {res}".format(res=part_two(inp)))
