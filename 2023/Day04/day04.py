2a02b64252726cd0641552b9adbe31048240d446
DainDwarf <daindwarf@gmail.com>
2023-12-04 11:05:50 +0100

    Refactor part one to prepare for part 2

--- 2023/Day04/day04.py
+++ 2023/Day04/day04.py
@@ -3,8 +3,16 @@ import pytest
 
 
 # That's handy, the Advent of Code gives unittests.
+TEST_EXAMPLE = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
+
+
 @pytest.mark.parametrize("inp, exp", [
-    ("41 48 83 86 17 | 83 86  6 31 17  9 48 53", 8),
+    ("41 48 83 86 17 | 83 86  6 31 17  9 48 53", 4),
     ("13 32 20 16 61 | 61 30 68 82 17 32 24 19", 2),
     (" 1 21 53 59 44 | 69 82 63 72 16 21 14  1", 2),
     ("41 92 73 84 69 | 59 84 76 51 58  5 54 83", 1),
@@ -16,11 +24,13 @@ def test_score(inp, exp):
     assert res == exp
 
 
-@pytest.mark.parametrize("inp, exp", [
-])
-def test_two(inp, exp):
-    res = part_two(inp)
-    assert res == exp
+def test_one():
+    assert part_one(TEST_EXAMPLE) == 13
+
+
+@pytest.mark.skip
+def test_two():
+    assert part_two(TEST_EXAMPLE) == 30
 
 
 def score(card):
@@ -28,18 +38,15 @@ def score(card):
     wins = [int(w) for w in wins.split()]
     nums = [int(n) for n in nums.split()]
 
-    ret = 0
-    for n in nums:
-        if n in wins:
-            ret += (ret if ret != 0 else 1)
-    return ret
+    return sum(n in wins for n in nums)
 
 
 def part_one(inp):
     ret = 0
     for line in inp.split('\n'):
         card = line.split(':')[1]
-        ret += score(card)
+        if s := score(card):
+            ret += 2**(s-1)
     return ret
 
 
